52682976b30f1dcdf70c7d356edafa85a00b3f3a
YOUR NAME <YOU@EXAMPLE.COM>
2020-08-30 20:58:16 +0800

    update

---
+++ 04src react-reduxAsync/App.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import CounterContainer from './containers/counter_container';
+export default class App extends Component {
+  render() {
+    return <CounterContainer />
+  }
+}
